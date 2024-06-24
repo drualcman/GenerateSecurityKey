827bac066b500c7a2c53f8ccabb211c3e24d2b64
Sergi Ortiz Gomez <sergi.ortiz.gomez@gluonconsulting.com>
2024-06-25 07:51:25 +0800

    Add project files.

---
+++ GenerateSecurityKey/Program.cs
@@ -0,0 +1,36 @@
+﻿using GenerateSecurityKey;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+
+int Keysize = 0;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine($"Minimum size is 32. ");
+    Console.Write($"Enter your key Size: ");
+    string Size = Console.ReadLine()!;
+
+    if(int.TryParse(Size, out Keysize))
+    {
+        if (Keysize > 31)
+        {
+            JwtOptions jwtOptions = new JwtOptions
+            {
+                SecurityKey = GenerateSecurityKey(Keysize)
+            };
+            Console.WriteLine($"Generated SecurityKey: {jwtOptions.SecurityKey}");
+            Console.WriteLine("Whould you like to continues? YES/NO");
+            string continues = Console.ReadLine();
+            if (continues.Equals("YES", StringComparison.InvariantCultureIgnoreCase)) Keysize = 0;
+        }
+    }
+} while (Keysize < 32);
+
+static SecurityKey GenerateSecurityKey(int keySize)
+{
+    byte[] key = new byte[keySize];
+    using RandomNumberGenerator rng = RandomNumberGenerator.Create();
+    rng.GetBytes(key);
+    return new SymmetricSecurityKey(key);
+}
